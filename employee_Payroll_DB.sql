89f2f598d6c12ff393824a81e3c33f6ef2f7a290
smsmaurya <sumeshmaurya786@gmail.com>
2023-12-07 14:54:02 +0530

    write queries and test to retrive data from database.

--- employee_Payroll_DB.sql
+++ employee_Payroll_DB.sql
@@ -37,7 +37,37 @@ WHERE start BETWEEN CAST('2018-01-01' AS DATE) AND DATE (NOW());
 
 ALTER TABLE employee_payroll ADD gender CHAR(1) AFTER name;
 
+SET SQL_SAFE_UPDATES = 0;
+ UPDATE employee_payroll SET gender = 'M' WHERE name='Bill' OR name='Charlie';
+ 
+ UPDATE employee_payroll SET gender = 'F' WHERE name='Tersia';
+ 
+ UPDATE employee_payroll SET gender = 'F' WHERE name='Tersia';
+ 
+
+ SELECT AVG(salary) FROM employee_payroll;
+ 
+ INSERT INTO employee_payroll (name, gender, salary, start) VALUES ('Jenny', 'F', 2300000.00, '2022-10-13');
+ 
+ SELECT AVG(salary) FROM employee_payroll WHERE gender='M' GROUP BY gender;
+ 
+ SELECT AVG(salary) FROM employee_payroll WHERE gender='F' GROUP BY gender;
+ 
+ SELECT gender,AVG(salary) FROM employee_payroll GROUP BY gender;
+ 
+ SELECT gender, SUM(salary) FROM employee_payroll GROUP BY gender;
+ 
+ SELECT gender, MAX(salary), MIN(salary) FROM employee_payroll GROUP BY gender;
+ 
+ SELECT COUNT(id) AS female_count FROM employee_payroll WHERE gender='F' GROUP BY gender;
+ 
+ desc employee_payroll;
+
 #Dropping gender field
+
 ALTER TABLE employee_payroll DROP gender;
+
 DROP DATABASE payroll_service;
+
+create database Employee_Payroll_DB;
 
